c55dbe57979e8ffd9f01fb6bf8f2a0d654597e8b
ZhangLinKang <1287838886@qq.com>
2018-10-21 20:58:14 +0800

    添加手机短信验证码发送计时

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -13,8 +13,8 @@
                     <div :class="{on:loginWay}">
                         <section class="login_message">
                             <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
-                            <button disabled="disabled" class="get_verification" :class="{right_phone: rightPhone}">
-                                获取验证码
+                            <button :disabled="!rightPhone" class="get_verification" :class="{right_phone: rightPhone}" @click.prevent="getCode()">
+                               {{computeTime>0 ? `已发送(${computeTime}s)`: '获取验证码'}}
                             </button>
                         </section>
                         <section class="login_verification">
@@ -61,11 +61,30 @@
             return {
                 loginWay: true,
                 phone: '',
+                computeTime : 0     //计时的时间
             }
         },
-        computed:{
-            rightPhone(){
+        computed: {
+            rightPhone () {
                 return /^1\d{10}$/.test(this.phone)
+            }
+        },
+        methods: {
+            getCode(){
+                //如果当前没有计时
+                if(!this.computeTime) {
+                    this.computeTime = 60;
+                    const intervalId = setInterval(()=>{
+                        this.computeTime --;
+                        if(this.computeTime <=0){
+                            //停止计时
+                            clearInterval(intervalId)
+                        }
+                    },1000)
+                }
+                //console.log('发送验证码');
+                //启动倒计时
+                //发送ajax请求（向制定手机号码发送短信验证码）
             }
         }
     }
